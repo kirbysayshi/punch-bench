860a08b6669f2b614ef8b82189ba03d9522242fa
Andrew Petersen <kirbysayshi@gmail.com>
2021-03-30 15:09:32 -0400

    fix copy/paste behavior, exports, and scoping

--- src/bench.ts
+++ src/bench.ts
@@ -3,54 +3,25 @@
 // This file cannot require anything, since it might get copy/pasted into a
 // browser later. It has to be self-contained.
 
-export interface BenchedFunction {
-  (done: () => void): void;
-}
-
-export type PunchBenchModule = typeof punch;
-
-export type OnFinished = (
-  table: string,
-  results: BenchResult[],
-  computed: { name: string; stats: Stats }[],
-  summaries: SummarizedStats
-) => void;
-
-type BenchResult = {
-  name: string;
-  durations: number[];
-};
-
-type Stats = {
-  min: number;
-  max: number;
-  mean: number;
-  median: number;
-  pct99: number;
-  pct95: number;
-  sum: number;
-};
-
-type SingleStatSummary = {
-  type: keyof Stats;
-  fastest: string;
-  slowest: string;
-  pctFaster: number;
-  percentFaster: string;
-  difference: string;
-  total: string;
-};
-
-type NamedStats = { name: string; stats: Stats };
-
-type SummarizedStats = { [Name in keyof Stats]: SingleStatSummary };
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./declarations.d.ts" />
 
 async function __bench(
-  fn: BenchedFunction,
+  fn: PunchBench.BenchedFunction,
   times: number,
-  timingFn: PunchBenchOptions["nowFn"],
+  timingFn: PunchBenchOptions["nowFn"]
 ) {
   const durations: number[] = [];
+  let frames = 0;
+  let ref = 0;
+
+  if (typeof window !== "undefined" && window.requestAnimationFrame) {
+    const doFrame = () =>
+      window.requestAnimationFrame(() => {
+        frames++;
+        ref = doFrame();
+      });
+  }
 
   for (let i = 0; i < times; i++) {
     await new Promise<void>((resolve) => {
@@ -63,26 +34,30 @@ async function __bench(
     });
   }
 
-  return durations;
+  if (typeof window !== "undefined" && window.cancelAnimationFrame) {
+    window.cancelAnimationFrame(ref);
+  }
+
+  return { durations, frames };
 }
 
 async function __compare(
-  tests: BenchedFunction[],
+  tests: PunchBench.BenchedFunction[],
   times: number,
-  timingFn: PunchBenchOptions["nowFn"],
+  timingFn: PunchBenchOptions["nowFn"]
 ) {
-  const results: BenchResult[] = [];
+  const results: PunchBench.BenchResult[] = [];
 
   for (let i = 0; i < tests.length; i++) {
     const testFn = tests[i]!;
-    const durations = await __bench(testFn, times, timingFn);
-    results.push({ name: testFn.name, durations });
+    const { durations, frames } = await __bench(testFn, times, timingFn);
+    results.push({ name: testFn.name, durations, frames });
   }
 
   return results;
 }
 
-function __minMaxMeanMedianPct99Pct95(times: number[]): Stats {
+function __minMaxMeanMedianPct99Pct95(times: number[], frames: number): PunchBench.Stats {
   const min = Math.min.apply(null, times);
   const max = Math.max.apply(null, times);
   const mean = times.reduce((sum, curr) => sum + curr, 0) / times.length;
@@ -91,21 +66,21 @@ function __minMaxMeanMedianPct99Pct95(times: number[]): Stats {
   const pct99 = sortedAsc[Math.round((times.length - 1) * 0.99)]!;
   const pct95 = sortedAsc[Math.round((times.length - 1) * 0.95)]!;
   const sum = times.reduce((sum, curr) => sum + curr, 0);
-  return { min, max, mean, median, pct99, pct95, sum };
+  return { min, max, mean, median, pct99, pct95, sum, frames };
 }
 
-function __summarize(namedStats: NamedStats[]) {
+function __summarize(namedStats: PunchBench.NamedStats[]) {
   return Object.keys(namedStats[0]!.stats)
-    .map((name) => statSummary(namedStats, name as keyof Stats))
+    .map((name) => statSummary(namedStats, name as keyof PunchBench.Stats))
     .reduce((all, curr) => {
       all[curr.type] = curr;
       return all;
-    }, {} as SummarizedStats);
+    }, {} as PunchBench.SummarizedStats);
 
   function statSummary(
-    namedStats: NamedStats[],
-    statName: keyof Stats
-  ): SingleStatSummary {
+    namedStats: PunchBench.NamedStats[],
+    statName: keyof PunchBench.Stats
+  ): PunchBench.SingleStatSummary {
     const values = namedStats.map(({ stats }) => stats[statName]);
     const total = values.reduce((all, v) => all + v, 0);
     const idxFastest = values.indexOf(Math.min.apply(null, values));
@@ -132,21 +107,21 @@ function __summarize(namedStats: NamedStats[]) {
   }
 }
 
-function __compute(results: BenchResult[]) {
+function __compute(results: PunchBench.BenchResult[]) {
   const stats = results.map((result) => ({
     name: result.name,
-    stats: __minMaxMeanMedianPct99Pct95(result.durations),
+    stats: __minMaxMeanMedianPct99Pct95(result.durations, result.frames),
   }));
   return stats;
 }
 
-function __asTable(summaries: SummarizedStats) {
+function __asTable(summaries: PunchBench.SummarizedStats) {
   const fieldNames = [
     "fastest",
     "percentFaster",
     "difference",
     "total",
-  ] as (keyof SingleStatSummary)[];
+  ] as (keyof PunchBench.SingleStatSummary)[];
 
   const colSize = 20;
   let out =
@@ -157,7 +132,7 @@ function __asTable(summaries: SummarizedStats) {
 
   out += out.replace(/./g, "-");
 
-  (Object.keys(summaries) as (keyof Stats)[]).forEach((statName) => {
+  (Object.keys(summaries) as (keyof PunchBench.Stats)[]).forEach((statName) => {
     const stats = summaries[statName];
     out +=
       "" +
@@ -189,7 +164,7 @@ const __envTimes = {
 };
 
 // GLOBAL STATE, YEAH IT'S NOT GREAT BUT IT'S SIMPLE.
-const __tests: BenchedFunction[] = [];
+const __tests: PunchBench.BenchedFunction[] = [];
 
 const __defaultOptions = {
   count: 1000,
@@ -205,7 +180,7 @@ type PunchBenchOptions = typeof __defaultOptions;
 
 let __opts: null | PunchBenchOptions = null;
 
-function punch(fn: BenchedFunction) {
+function punch(fn: PunchBench.BenchedFunction) {
   if (!fn) throw new Error("Expected a function, instead got " + fn);
   __tests.push(fn);
 }
@@ -214,9 +189,9 @@ punch.configure = function (opts: Partial<PunchBenchOptions>) {
   __opts = Object.assign({}, __defaultOptions, opts);
 };
 
-punch.go = function (cb?: OnFinished) {
+punch.go = function (cb?: PunchBench.OnFinished) {
   if (!__opts) throw new Error(".configure has not been called! Abort.");
-  __compare(__tests, __opts.count, __opts.nowFn).then(results => {
+  __compare(__tests, __opts.count, __opts.nowFn).then((results) => {
     const computed = __compute(results);
     const summaries = __summarize(computed);
     const table = __asTable(summaries);
@@ -232,7 +207,7 @@ punch.reset = function () {
 // Kick off initial configuration population.
 punch.configure({});
 
-if (typeof module !== "undefined") {
+if (typeof module !== "undefined" && "exports" in module) {
   module.exports = punch;
 }
 
---
+++ src/declarations.d.ts
@@ -0,0 +1,50 @@
+// declare namespace PunchBench {
+
+export as namespace PunchBench;
+
+export interface BenchedFunction {
+  (done: () => void): void;
+}
+
+export type PunchBenchModule = typeof punch;
+
+export type OnFinished = (
+  table: string,
+  results: BenchResult[],
+  computed: { name: string; stats: Stats }[],
+  summaries: SummarizedStats
+) => void;
+
+export type BenchResult = {
+  name: string;
+  durations: number[];
+  frames: number;
+};
+
+export type Stats = {
+  min: number;
+  max: number;
+  mean: number;
+  median: number;
+  pct99: number;
+  pct95: number;
+  sum: number;
+  frames: number; // higher is better
+  // eventLoops: number; // higher is better
+};
+
+export type SingleStatSummary = {
+  type: keyof Stats;
+  fastest: string;
+  slowest: string;
+  pctFaster: number;
+  percentFaster: string;
+  difference: string;
+  total: string;
+};
+
+export type NamedStats = { name: string; stats: Stats };
+
+export type SummarizedStats = { [Name in keyof Stats]: SingleStatSummary };
+
+// }
